060ab0f65aabd2a8b54b8cbf740e60a0eb6d90ca
eduardom <xmartinez@stuba.sk>
2021-03-20 22:45:36 +0100

    Test

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -15,7 +15,7 @@ const burger = (props) => {
         }, []); // reduce - transform an array into something else
     
     if (transformedIngredients.length === 0){
-        transformedIngredients = <p>Please start adding ingredients</p>
+        transformedIngredients = <p>Please start adding ingredients!</p>
     }
 
     return (
